81c0bbf23d1c111a708e98dafc57a3bd8d0da4d5
raymond <raymondomon@yahoo.com>
2022-03-02 16:03:11 +0000

    sonar cube config

--- src/main/java/com/password/PasswordValidator.java
+++ src/main/java/com/password/PasswordValidator.java
@@ -1,12 +1,9 @@
 package com.password;
 
-public class PasswordValidator {
-    public boolean isValid(String password) {
-        if(password.length() >=5 && password.length() <=10) {
-            return true;
-        }else {
-            return false;
-        }
+ class PasswordValidator {
+   public boolean isValid(String password) {
+      return password.length() >=5 && password.length() <=10;
+
     }
 
 }
