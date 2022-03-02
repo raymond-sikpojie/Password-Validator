5db83467d0005c637e1dc7deb164d34bb83d6fa5
raymond <raymondomon@yahoo.com>
2022-03-02 16:07:38 +0000

    code refactor

--- src/test/java/com/password/TestPassword.java
+++ src/test/java/com/password/TestPassword.java
@@ -3,9 +3,9 @@ package com.password;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-public class TestPassword {
+class TestPassword {
 @Test
-    public void TestPasswordLength() {
+     void TestPasswordLength() {
     PasswordValidator pv = new PasswordValidator();
     Assert.assertEquals(true, pv.isValid("Abc12"));
 
