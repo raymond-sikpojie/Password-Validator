496db815354f14bbceb55c0fd6e34196566ab0c8
raymond <raymondomon@yahoo.com>
2022-03-03 09:58:06 +0000

    code refactor

--- src/test/java/com/password/TestPassword.java
+++ src/test/java/com/password/TestPassword.java
@@ -3,7 +3,7 @@ package com.password;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-class TestPassword {
+public class TestPassword {
 @Test
      void TestPasswordLength() {
     PasswordValidator pv = new PasswordValidator();
